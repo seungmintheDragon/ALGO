6a29fc08e1a0eb728dbd34030a0380d18b8a783b
S_M_DRAGON <103255854+seungmintheDragon@users.noreply.github.com>
2023-10-12 15:06:40 +0900

    [Gold V] Title: 최소비용 구하기, Time: 116 ms, Memory: 4628 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <queue>
+#define INF 10e9
+
+using namespace std;
+
+int N, M;
+vector<pair<int, int>> nodes[1001];
+int S, E;
+int dist[1001];
+
+void input()
+{
+	cin >> N >> M;
+
+	for (int i = 0; i < M; i++)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		nodes[a].push_back({ b,c });
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		dist[i] = INF;
+	}
+	cin >> S >> E;
+}
+
+void Dijkstra()
+{
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+	pq.push({ 0, S });
+	dist[S] = 0;
+	while (!pq.empty())
+	{
+		int cost = pq.top().first;
+		int current = pq.top().second;
+		pq.pop();
+
+		if (dist[current] < cost) continue;
+
+		for (pair<int, int> node : nodes[current])
+		{
+			int next = node.first;
+			int next_cost = node.second;
+
+			if (dist[next] > cost + next_cost)
+			{
+				dist[next] = cost + next_cost;
+				pq.push({ dist[next], next });
+			}
+		}
+	}
+
+}
+
+int main()
+{
+	input();
+	Dijkstra();
+	cout << dist[E] << endl;
+	return 0;
+}
