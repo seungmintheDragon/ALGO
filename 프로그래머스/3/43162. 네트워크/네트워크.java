1e73c9634829f6066c792f8c9b248c6659e31ebc
S_M_DRAGON <103255854+seungmintheDragon@users.noreply.github.com>
2024-04-22 17:13:33 +0900

    [level 3] Title: 네트워크, Time: 0.10 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,29 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Solution {
+    boolean[] check;
+    
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        check = new boolean[n];
+              
+        for(int i = 0; i < computers.length; i ++) {
+            if(!check[i]) {
+                DFS(i, computers);
+                answer ++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    void DFS(int current, int[][] computers) {
+        check[current] = true;
+        for(int i =0; i < computers.length; i ++) {
+            if(i != current && !check[i] && computers[current][i] == 1 && computers[i][current] == 1) {
+                DFS(i, computers);
+            }
+        }
+    }
+}
