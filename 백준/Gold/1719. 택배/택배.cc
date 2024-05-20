62f2882ecdad092711cdc2053a369d5018c412b6
S_M_DRAGON <103255854+seungmintheDragon@users.noreply.github.com>
2024-05-21 08:01:30 +0900

    [Gold III] Title: 택배, Time: 52 ms, Memory: 2608 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.cc
+++ 백준/Gold/1719. 택배/택배.cc
@@ -2,63 +2,62 @@
 #include <algorithm>
 #include <vector>
 #include <queue>
-#define MAX 987654321
- 
+#define INF 987654321
+
 using namespace std;
- 
-int n, m, dis, pos, ndis, npos; // n = 1번부터 n번까지 집하장 개수
+
+int n, m, dis, pos, ndis, npos;
 vector<pair<int, int>> map[201];
 int ans[201][201];
-int stp[201][201];
- 
- 
-void delivery(int from) {
-    for (int i = 1; i <= n; i++) ans[from][i] = MAX, stp[from][i] = MAX;
+int tmp[201][201];
+
+void dijkstra(int from) {
+    for (int i = 1; i <= n; i++) {
+        ans[from][i] = INF;
+        tmp[from][i] = INF;
+    }
     ans[from][from] = 0;
-    stp[from][from] = 0;
+    tmp[from][from] = 0;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({ 0,from });
+    pq.push({ 0, from });
     while (!pq.empty()) {
-        dis = pq.top().first; // first 는 누적 거리
-        pos = pq.top().second; // second는 현 위치
-        pq.pop(); 
+        dis = pq.top().first;
+        pos = pq.top().second;
+        pq.pop();
         for (int i = 0; i < map[pos].size(); i++) {
             npos = map[pos][i].first;
             ndis = map[pos][i].second;
             if (ans[from][npos] > ans[from][pos] + ndis) {
                 ans[from][npos] = ans[from][pos] + ndis;
-                if(stp[from][pos]==0) stp[from][npos] = npos;
-                else stp[from][npos] = stp[from][pos];
-                pq.push({ ans[from][npos],npos });
+                if (tmp[from][pos] == 0) tmp[from][npos] = npos;
+                else tmp[from][npos] = tmp[from][pos];
+
+                pq.push({ ans[from][npos], npos });
             }
         }
     }
 }
- 
-void input() {
-    int to, from, dis;
+
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     cin >> n >> m;
-    while (m--) {
+    for (int i = 0; i < m; i++) {
+        int to, from, dis;
         cin >> to >> from >> dis;
-        map[to].push_back({ from,dis });
-        map[from].push_back({ to,dis });
+        map[to].push_back({ from, dis });
+        map[from].push_back({ to, dis });
     }
-}
- 
-void output() {
+
+    for (int i = 1; i <= n; i++) dijkstra(i);
+
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            if (i == j) cout << "-" << " ";
-            else cout << stp[i][j] << " ";
+            if (i == j) cout << "- ";
+            else cout << tmp[i][j] << " ";
         }
         cout << "\n";
     }
-}
- 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    input();
-    for (int i = 1; i <= n; i++) delivery(i);
-    output();
+
     return 0;
 }
