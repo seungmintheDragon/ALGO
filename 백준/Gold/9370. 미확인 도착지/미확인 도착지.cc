3b1c936691e2906cf7d8bdc44ef3ddd426b71b05
S_M_DRAGON <103255854+seungmintheDragon@users.noreply.github.com>
2024-05-21 07:11:11 +0900

    [Gold II] Title: 미확인 도착지, Time: 36 ms, Memory: 4448 KB -BaekjoonHub

---
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.cc
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <math.h>
+#pragma warning(disable:4996)
+
+using namespace std;
+#define INF 1234567890
+
+struct Node {
+	int end;
+	int cost;
+	Node(int X, int Y) :end(X), cost(Y) {}
+};
+struct compare {
+	bool operator()(Node a, Node b) {
+		return a.cost > b.cost;
+	}
+};
+
+
+vector<Node> vertex[2001];
+vector<int> destination;
+vector<int> Dist_S;
+vector<int> Dist_G;
+vector<int> Dist_H;
+
+void Dijkstra(int n, int start, vector<int> &dist) {
+	priority_queue<Node, vector<Node>, compare> pq;
+	dist.assign(n + 1, INF);
+	dist[start] = 0;
+	pq.push(Node(start, dist[start]));
+
+	while (!pq.empty()) {
+		Node temp = pq.top();
+		int cur = temp.end;
+		int cost = temp.cost;
+		pq.pop();
+
+		for (int i = 0; i < vertex[cur].size(); i++) {
+			temp = vertex[cur][i];
+			int next = temp.end;
+			int next_cost = cost + temp.cost;
+			if (dist[next] > next_cost) {
+				dist[next] = next_cost;
+				pq.push(Node(next, next_cost));
+			}
+		}
+	}
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		int n, m, t;
+		cin >> n >> m >> t;
+		int s, g, h;
+		cin >> s >> g >> h;
+
+		for (int i = 0; i < m; i++) {
+			int start, end, cost;
+            cin >> start >> end >> cost;
+			vertex[start].emplace_back(Node(end, cost));
+			vertex[end].emplace_back(Node(start, cost));
+		}
+
+		for (int i = 0; i < t; i++) {
+			int dest;
+			cin >> dest;
+			destination.emplace_back(dest);
+		}
+        
+		sort(destination.begin(), destination.end());
+
+		Dijkstra(n, s, Dist_S);
+		Dijkstra(n, g, Dist_G);
+		Dijkstra(n, h, Dist_H);
+
+		for (int i = 0; i < destination.size(); i++) {
+			int Dest = destination[i];
+			if (Dist_S[Dest] == Dist_S[g] + Dist_G[h] + Dist_H[Dest])
+				cout << Dest << ' ';
+			else if (Dist_S[Dest] == Dist_S[h] + Dist_G[h] + Dist_G[Dest])
+				cout << Dest << ' ';
+		}
+		cout << '\n';
+
+		for (int i = 0; i <= n; i++)
+			vertex[i].clear();
+
+		destination.clear();
+		Dist_S.clear();
+		Dist_G.clear();
+		Dist_H.clear();
+	}
+
+	return 0;
+}
