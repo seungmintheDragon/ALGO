88162367132fb323d2da69dc079caa0c08981104
S_M_DRAGON <103255854+seungmintheDragon@users.noreply.github.com>
2024-11-02 01:16:11 +0900

    [Gold V] Title: 최소비용 구하기, Time: 112 ms, Memory: 22824 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cs
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cs
@@ -1,74 +1,69 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace Baekjoon
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        public class MyCompare : IComparer<List<int>>
+        const int INF = int.MaxValue / 2;
+        int N = int.Parse(Console.ReadLine());  // 도시의 개수
+        int M = int.Parse(Console.ReadLine());  // 버스의 개수
+
+        // 인접 행렬 초기화
+        int[,] graph = new int[N + 1, N + 1];
+        for (int i = 1; i <= N; i++)
         {
-            public int Compare(List<int> x, List<int> y)
+            for (int j = 1; j <= N; j++)
             {
-                if(x[0] == y[0]) return x[1] - y[1];
-                return x[0] - y[0];
+                if (i != j) graph[i, j] = INF;
             }
         }
-        
-        static int N;
-        static int M;
-        static int S;
-        static int E;
-        static int[] dist = new int[1001];
-        static List<List<int>>[] nodes = new List<List<int>>[1001];        
-        
-        public static void Dijkstra()
+
+        // 간선 입력
+        for (int i = 0; i < M; i++)
         {
-            SortedSet<List<int>> pq = new SortedSet<List<int>>(new MyCompare());
-            dist[S] = 0;
-            pq.Add(new List<int>() {0, S});
-            while(pq.Count != 0)
+            int[] busInfo = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int from = busInfo[0];
+            int to = busInfo[1];
+            int cost = busInfo[2];
+            graph[from, to] = Math.Min(graph[from, to], cost);
+        }
+
+        int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int start = input[0];
+        int end = input[1];
+
+        // 다익스트라
+        int[] minDist = new int[N + 1];     // 시작 노드에서 각 정점까지의 최단 경로
+        bool[] visited = new bool[N + 1];   // 방문 여부 체크(가까운 노드부터 방문)
+        for(int i = 1; i <= N; i++)
+        {
+            minDist[i] = INF;
+        }
+        minDist[start] = 0; // 시작 노드에서 시작 노드로 가는 거리는 0
+
+        for (int i = 1; i <= N; i++)
+        {
+            int u = -1; // 현재까지 처리한 노드 중에서 최단 거리에 해당하는 노드 저장
+            for (int j = 1; j <= N; j++)
             {
-                List<int> costcur = pq.First();
-                pq.Remove(costcur);
-                int cost = costcur[0];
-                int cur = costcur[1];
-                if(dist[cur] < cost) continue;
-                
-                for(int i = 0; i < nodes[cur].Count; i ++)
+                if (!visited[j] && (u == -1 || minDist[j] < minDist[u]))
                 {
-                    int next = nodes[cur][i][0];
-                    int nc = cost + nodes[cur][i][1];
-                    if(dist[next] > nc)
-                    {
-                        dist[next] = nc;
-                        pq.Add(new List<int>() {nc, next});
-                    }
+                    u = j; // 방문하지 않은 노드 중 가장 가까운 노드를 u에 저장
+                }
+            }
+
+            if (minDist[u] == INF) break;   // 접근 가능한 노드가 없다면 break
+
+            visited[u] = true;
+
+            // 현재 노드 u에서 접근 가능한 다른 노드 순회
+            for (int v = 1; v <= N; v++)
+            {
+                if (!visited[v] && graph[u, v] < INF)
+                {
+                    minDist[v] = Math.Min(minDist[v], minDist[u] + graph[u, v]);
                 }
             }
         }
-        
-        
-        static void Main(String[] args)
-        {
-            int[] input;
-            N = int.Parse(Console.ReadLine());
-            M = int.Parse(Console.ReadLine());
-            for(int i = 0; i <= N; i ++) 
-            {    
-                 nodes[i] = new List<List<int>>();
-                 dist[i] = (int)1e9;
-            }
-            for(int i = 0; i < M; i ++) {
-                input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-                nodes[input[0]].Add(new List<int>{input[1], input[2]});
-            }
-            input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-            S = input[0]; E = input[1];
-            Dijkstra();
-            Console.WriteLine(dist[E]);
-            
-        }
+
+        Console.WriteLine(minDist[end]);
     }
 }
